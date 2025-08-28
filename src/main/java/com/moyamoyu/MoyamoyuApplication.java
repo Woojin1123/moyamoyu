67d70ff4ef3488cfef3cca651e72154c90fd5f95
Woojin1123 <boom175839@gmail.com>
2025-08-28 16:54:36 +0900

    init commit

---
+++ src/main/java/com/moyamoyu/MoyamoyuApplication.java
@@ -0,0 +1,13 @@
+package com.moyamoyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoyamoyuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoyamoyuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moyamoyu/MoyamoyuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moyamoyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoyamoyuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

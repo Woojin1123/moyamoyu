dbfe0daf6e6e355c25df91f43205df987e5f844e
Woojin1123 <boom175839@gmail.com>
2025-09-04 17:56:04 +0900

    feat: 인증된 유저의 정보를 담을 DTO 생성

---
+++ src/main/java/com/moyamoyu/dto/AuthUser.java
@@ -0,0 +1,15 @@
+package com.moyamoyu.dto;
+
+import com.moyamoyu.entity.ServiceRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class AuthUser {
+    private Long id;
+    private String email;
+    private ServiceRole role;
+}

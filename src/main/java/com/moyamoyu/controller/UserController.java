fe2b5c6f85af94b0691229e255cbe851d65665dd
Woojin1123 <boom175839@gmail.com>
2025-09-10 18:19:22 +0900

    comment: 유저 컨트롤러 Swagger 명세 작성

--- src/main/java/com/moyamoyu/controller/UserController.java
+++ src/main/java/com/moyamoyu/controller/UserController.java
@@ -7,6 +7,8 @@ import com.moyamoyu.dto.request.UserInfoPatchRequest;
 import com.moyamoyu.dto.response.MyUserInfoResponse;
 import com.moyamoyu.dto.response.UserSimpleResponse;
 import com.moyamoyu.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,10 +17,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
+@Tag(name = "UserController" , description = "유저 정보 관련 API")
 public class UserController {
     private final UserService userService;
 
     @GetMapping("/me")
+    @Operation(summary = "내 정보 조회", description = "인증된 사용자에 대한 정보를 조회하는 API")
     public ResponseEntity<ApiResponse<MyUserInfoResponse>> getMyInfo(
             @AuthenticationPrincipal AuthUser authUser
     ){
@@ -31,6 +35,7 @@ public class UserController {
     }
 
     @GetMapping("/{userId}")
+    @Operation(summary = "유저 정보 조회", description = "유저의 정보를 조회하는 API")
     public ResponseEntity<ApiResponse<UserSimpleResponse>> getUserInfo(
             @PathVariable Long userId
     ){
@@ -43,6 +48,7 @@ public class UserController {
     }
 
     @PatchMapping("/me")
+    @Operation(summary = "내 정보 수정", description = "인증된 사용자에 대한 정보를 수정하는 API")
     public ResponseEntity<ApiResponse<MyUserInfoResponse>> patchUserInfo(
             @AuthenticationPrincipal AuthUser authUser,
             @RequestBody UserInfoPatchRequest userInfoPatchRequest
@@ -56,6 +62,7 @@ public class UserController {
     }
 
     @DeleteMapping("/me")
+    @Operation(summary = "회원탈퇴", description = "인증된 사용자에 대한 회원탈퇴 API")
     public ResponseEntity<ApiResponse<Long>> deleteUser(
             @AuthenticationPrincipal AuthUser authUser,
             @RequestBody UserDeleteRequest userDeleteRequest
--- src/main/java/com/moyamoyu/dto/AuthUser.java
+++ src/main/java/com/moyamoyu/dto/AuthUser.java
@@ -1,6 +1,7 @@
 package com.moyamoyu.dto;
 
 import com.moyamoyu.entity.enums.ServiceRole;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,8 +9,12 @@ import lombok.Getter;
 @Getter
 @Builder
 @AllArgsConstructor
+@Schema(description = "Security 인증 객체")
 public class AuthUser {
+    @Schema(description = "인증된 유저 ID")
     private Long id;
+    @Schema(description = "인증된 유저 이메일")
     private String email;
+    @Schema(description = "인증된 유저 역할")
     private ServiceRole role;
 }

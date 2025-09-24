ded09df6cb5b902b4abbd47befb7426993a63816
Woojin1123 <boom175839@gmail.com>
2025-09-24 22:28:45 +0900

    refactor: 커버 이미지, 모임 정책 응답값 추가

--- src/main/java/com/moyamoyu/dto/response/SimpleMoimResponse.java
+++ src/main/java/com/moyamoyu/dto/response/SimpleMoimResponse.java
@@ -18,7 +18,12 @@ public record SimpleMoimResponse(
         @Schema(description = "모임 현재 인원")
         Long memberCount,
         @Schema(description = "카테고리")
-        String category
+        String category,
+        @Schema(description = "커버이미지")
+        String coverImgUrl,
+        @Schema(description = "모임 정책")
+        String policy
+
 ) {
     public static SimpleMoimResponse from(Moim moim){
         return new SimpleMoimResponse(
@@ -27,7 +32,9 @@ public record SimpleMoimResponse(
                 moim.getDescription(),
                 moim.getGrade().getCapacity(),
                 moim.getMemberCount(),
-                moim.getCategory().name()
+                moim.getCategory().name(),
+                moim.getCoverImageUrl(),
+                moim.getJoinPolicy().name()
         );
     }
 }
